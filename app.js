98182787fbdd4248a8bf8232178645597c9765aa
Samuel Palacios <pal20010@byui.edu>
2022-11-02 00:11:34 -0600

    Personal Assignment Week 07

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Import Node Libraries
+const { auth, requiresAuth } = require('express-openid-connect');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -11,14 +12,29 @@ const port = process.env.PORT || 8090;
 // Instance of express API
 const myApp = express();
 
+// For OAuth 2.0
+const dotenv = require('dotenv')
+dotenv.config();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
 myApp.use(bodyParser.json())
     .use((req, res, next) => {
         // Preparing headings
         res.setHeader('Access-Control-Allow-Origin', '*');
         next();
     })
-    // Redirect to routes
-    .use('/', require('./routes'));
+    .use(auth(config))    
+    // Redirect to routes       
+    .use('/', requiresAuth(), require('./routes'),  
+    );
 
 // Staring database access service    
 mongodb.initDb((err, mongodb) => {
@@ -31,4 +47,4 @@ mongodb.initDb((err, mongodb) => {
     myApp.listen(port);
     console.log(`Connected to DB and listening on ${port}`);
   }
-});
+})
